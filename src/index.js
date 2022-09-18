947062933b2c2da6613c3a6017defcf1c5d74766
skee2 <hellosk5@naver.com>
2022-09-18 17:37:01 +0900

    useState(), useEffect()

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Clock from './chapter_04/clock';
 import CommentList from './chapter_05/CommentList';
+import Counter from './chapter_06/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <Counter />
   </React.StrictMode>
 );
 
