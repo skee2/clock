ab16f4542250aedbaa6c6a6deccd912e8d02a5e8
skee2 <hellosk5@naver.com>
2022-10-20 17:30:08 +0900

    submit form

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import Counter from './chapter_06/Counter';
 import Accommodate from './chapter_07/Accommodate';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <SignUp />
   </React.StrictMode>
 );
 
